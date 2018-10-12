614cf2893cee8f0dc88298fc905125e800b83847
Klas Holmgren <Klas Holmgren>
2018-10-12 12:33:09 +0200

    lists

--- cell_distance.c
+++ cell_distance.c
@@ -3,6 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
+float distence3D(int* x, int* y){
+
+  int dist = (x[0]-y[0])*(x[0]-y[0]) + (x[1]-y[1]])*(x[1]]-y[1]]) + (x[2]-y[2]])*(x[2]]-y[2]]);
+  return sqrt((float)dist)/1000;
+  //Avrunda
+
+}
 
 int main(int argc, char *argv[]) {
 
@@ -10,24 +19,6 @@ int main(int argc, char *argv[]) {
 
   int n_threads = 0;
 
-
-/*
-  for(int i = 1; i < argc; i++){
-
-    str = argv[i];
-
-    ret = strcmp(*str[1], "t");
-
-    printf("%s",str[1]);
-
-    if(0 == ret){
-      printf("hittat n_threads\n");
-      n_threads = (int)strtol(argv[i]+2, (char **)NULL,10);
-    }
-  }
-
-  printf("Kör med %i threads \n",n_threads);
-*/
   //Read coordinates -10 to 10 from cells.txt
 
   FILE * fp = fopen("cell_data/cell_50","r");
@@ -39,6 +30,34 @@ int main(int argc, char *argv[]) {
   printf("%s\n", buffer);
 
   //calculate distances count frequency rouded to 2 didgets
+
+
+  for(int i = 0; i < len(cell_Cord); i++){
+    //dont want to comapre with it self therfor j=(i+1)
+    for(int j = (i+1); j < len(cell_Cord); j++){
+      distance[i] = distence3D(cell_Cord[i],cell_Cord[j]);
+    }
+  }
+
+  int* dist;
+  int* Frequens;
+
+  for(int i = 0; len(distance[1]); i++)){
+    for(int j = 0; len(distance[1]); j++){
+      //Dont want to comapre
+      //if in list increase freq
+      if(distFrequens[0][j] == distance[i]){
+        Frequens[j]++;
+        break;
+      }
+      //else append to bottom of list
+      //Can se if we got to bottom by checking for null or something
+      if(distFrequens[0][j] == -1){
+        dist[j] = distance[i];
+        Frequens[j] = 1;
+      }
+    }
+  }
 
   //Outbut (stdout) sorted list of distance eith freq.
 
