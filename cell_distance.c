a46f3f4c60f61d18c4df39dc00e9f7291f9f6c25
Amanda <amandasjoland@hotmail.com>
2018-10-12 12:31:34 +0200

    läste id data

--- cell_distance.c
+++ cell_distance.c
@@ -1,43 +1,49 @@
-#include <omp.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<omp.h>
+#include<stddef.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+struct timespec start;
+#define CHECKPOINT(fmt, ...) { \
+struct timespec now; \
+timespec_get(&now, TIME_UTC); \
+float elapsed = (now.tv_sec + 1.0e-9 * now.tv_nsec) - (start.tv_sec + 1.0e-9 * start.tv_nsec); \
+printf("\n%f: ", elapsed); \
+printf(fmt, ##__VA_ARGS__); \
+}
 
 int main(int argc, char *argv[]) {
+  timespec_get(&start, TIME_UTC);
+  CHECKPOINT("Started\n");
 
-  //Läs inn argumnet
+  // läser in variabler
+  char * endpt;
+  int tn = strtol(argv[1]+2, &endpt,10);
+  printf("Kör med %i threads \n",tn);
 
-  int n_threads = 0;
+  // hittar antal rader indatan är
+  FILE * fp = fopen("cell_data/cell_e5","r");
+  fseek(fp, 0L, SEEK_END);
+  int sz = ftell(fp);
+  fseek(fp, 0L, SEEK_SET);
+  int rows = sz/24;
+  printf("Filstorlek: %i på %i rader\n",sz, rows);
 
+  // allokerar minne till data
+  float* data = (float *)malloc(rows*sizeof(float)*3);
 
-/*
-  for(int i = 1; i < argc; i++){
-
-    str = argv[i];
-
-    ret = strcmp(*str[1], "t");
-
-    printf("%s",str[1]);
-
-    if(0 == ret){
-      printf("hittat n_threads\n");
-      n_threads = (int)strtol(argv[i]+2, (char **)NULL,10);
+  // läser in data
+  CHECKPOINT("Loading data\n");
+  for(size_t ix = 0; ix<rows*3; ix++)
+    {
+      fscanf(fp, "%f" ,&data[ix]);
+      //printf("%f \n", data[ix]);
     }
-  }
-
-  printf("Kör med %i threads \n",n_threads);
-*/
-  //Read coordinates -10 to 10 from cells.txt
-
-  FILE * fp = fopen("cell_data/cell_50","r");
-
-  char buffer[100];
-
-  (void) fread(buffer, 7*sizeof(char)+1, 10, fp);
-
-  printf("%s\n", buffer);
-
+  CHECKPOINT("Finished loading data\n");
+  fclose(fp);
+  
   //calculate distances count frequency rouded to 2 didgets
 
   //Outbut (stdout) sorted list of distance eith freq.
