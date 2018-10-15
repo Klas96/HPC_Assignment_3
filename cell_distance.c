8586bddb5659f073f79a9dc2d54979adda7807e2
Klas Holmgren <Klas Holmgren>
2018-10-15 08:26:05 +0200

    Hej

--- cell_distance.c
+++ cell_distance.c
@@ -9,27 +9,9 @@
 //float distence3D(float* x, float* y) {
 int distence3D(float x1, float x2, float x3, float y1, float y2, float y3) {
   //int dist = (x[0]-y[0])*(x[0]-y[0]) + (x[1]-y[1])*(x[1]-y[1]) + (x[2]-y[2])*(x[2]-y[2]);
-  int dist = (x1-y1)*(x1-y1) + (x2-y2)*(x2-y2) + (x3-y3)*(x3-y3);
-  return ((int)sqrt(dist)*100);
-  //Avrunda??
-}
-
-/*
-void distAll() {
-
-  float * distance = (float *)malloc(rows*rows*sizeof(float));
-
-  for(int i = 0; i < rows; i++){
-    for(int j = (i+1); j < rows; j++){
-      distance[i+j-1] = distence3D(data_p[i*3 + 0] , data_p[i*3 + 1], data_p[i*3 + 2], data_p[j*3 + 0], data_p[j*3 + 1], data_p[j*3 + 2]);
-    }
-  }
-}
-*/
-
-void sort(/* arguments */) {
-  /* code */
-}
+  float dist = (x1-y1)*(x1-y1) + (x2-y2)*(x2-y2) + (x3-y3)*(x3-y3);
+  return ((int)(sqrt(dist)*100));
+};
 
 int main(int argc, char *argv[]) {
 
@@ -38,72 +20,89 @@ int main(int argc, char *argv[]) {
   int tn = strtol(argv[1]+2, &endpt,10);
   //export OMP_NUM_THREADS=tn;
   omp_set_num_threads(tn);
-  printf("Kör med %i threads \n",tn);
 
-  // hittar antal rader indatan är
-  FILE * fp = fopen("cell_data/cell_e2","r");
-
-  fseek(fp, 0L, SEEK_END);
-  int sz = ftell(fp);
-  fseek(fp, 0L, SEEK_SET);
-  int rows = sz/24;
-  printf("Filstorlek: %i på %i rader\n",sz, rows);
-
-  	//rows*4 byte Storlek på inläsning.
+  FILE * fp1 = fopen("cell_data/cell_e5","r");
+  FILE * fp2 = fopen("cell_data/cell_e5","r");
 
   // allokerar minne till data
 
-  //float** data_pp = (float **)malloc(rows);
-  float* data_p = (float *)malloc(rows*sizeof(float)*3);//tidigare
-
-  // läser in data
+  int max_load = 10000; //max data memory
 
 
-  for(size_t ix = 0; ix<rows*3; ix++){
-        fscanf(fp, "%f" ,&data_p[ix]);
-        //printf("%f \n", data[ix]);
-    }
+  //data
+  float* data_p1 = (float *)malloc(max_load/2);
+  float* data_p2 = (float *)malloc(max_load/2);
 
-  fclose(fp);
-
-  //calculate distances count frequency rouded to 2 didgets
-
+  //resultat
   int dist_loc;
+  int* dist = (int*)malloc((3464)*sizeof(int));
+  int* Frequens = (int*)malloc((3464)*sizeof(int));
 
-  int* dist = (int*)malloc((1733)*sizeof(int));
-  int* Frequens = (int*)malloc((1733)*sizeof(int));
-
-  for(int i = 0; i < 1733; i++){
+  for(int i = 0; i < 3464; i++){
     //dist[i] = i*0.01;//Float
     dist[i] = i;//Int
     Frequens[i] = 0;
   }
 
+  int blocknr = 0;
 
-  #pragma omp parallel shared(Frequens)
-  {
-  for(int i = 0; i < rows; i++){
-    //dont want to comapre with it self therfor j=(i+1)
-    int nx = omp_get_thread_num();
-    printf("Thread %i: I take row %i \n",nx,i);
-    for(int j = (i+1); j < rows; j++){
-      dist_loc = distence3D(data_p[i*3 + 0] , data_p[i*3 + 1], data_p[i*3 + 2], data_p[j*3 + 0], data_p[j*3 + 1], data_p[j*3 + 2]);
-      for(int k = 0; k < 1733; k++){
-        if(dist[k] == dist_loc){
-          Frequens[k]++;
-          break;
+  char* etc;
+
+  //while(ftell(fp1) != EOF){
+  while (fscanf(fp1, "   %s   ", &etc) != EOF) {
+    printf("Börjar med block %i \n", blocknr);
+    blocknr++;
+    // läser in data 1
+    for(int i = 0; i<max_load/(sizeof(float)*2); i++){
+
+      if(ftell(fp1) != '-1L'){
+        fscanf(fp1, "%f" ,&data_p1[i]);
+        //printf("%f\n", data_p1[i]);
+
+      }else{break;}
+    }
+
+    //läser in data 2
+    for(int j = 0; j<max_load/(sizeof(float)*2); j++){
+      if(ftell(fp2) != '-1L'){
+        fscanf(fp2, "%f" ,&data_p2[j]);
+      }else{break;}
+    }
+
+    //jämför data 1 o data 2
+    #pragma omp parallel shared(Frequens)
+    {
+    for(int k = 0; k < max_load/(sizeof(float)*2); k+=3){
+
+      for(int l = (k+1); l < max_load/(sizeof(float)*2); l+=3){
+
+        dist_loc = distence3D(data_p1[k*3 + 0] , data_p1[k*3 + 1], data_p1[k*3 + 2], data_p2[l*3 + 0], data_p2[l*3 + 1], data_p2[l*3 + 2]);
+
+        //Jämför dist_loc och ökar den frekvensen
+        for(int m = 0; m < 3464; m++){
+          if(dist[m] == dist_loc){
+            Frequens[m]++;
+            break;
+          }
         }
       }
     }
+    }
   }
-  }
+
+  fclose(fp1);
+  fclose(fp2);
+
+  //calculate distances count frequency rouded to 2 didgets
+
+
   //Outbut (stdout) sorted list of distance eith freq.
 
   //Note: never use more that 1 GB memory
 
-  for(int i = 0; i < 1733; i++){
+  for(int i = 0; i < 3464; i++){
     if(Frequens[i] != 0){
-      printf("R = %i ",dist[i]);
+      printf("R = %f ",dist[i]*0.01);
       printf("F = %i \n",Frequens[i]);
     }
   }
