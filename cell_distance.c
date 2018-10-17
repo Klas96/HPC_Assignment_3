0eeb43b04dd3c97782c7b1d82a11ec32120c5eab
Klas Holmgren <Klas Holmgren>
2018-10-17 10:10:49 +0200

    hje

--- cell_distance.c
+++ cell_distance.c
@@ -8,13 +8,15 @@
 
 //parametrar
 
-int* Frequens;
-int* dist;
+//int* Frequens;
+//int* dist;
 
 float y_loc[3];
 
 
 float data;
+
+int Frequens[3465];
 
 //float distence3D(float* x, float* y) {
 int distence3D(float* X, float* Y) {
@@ -28,13 +30,14 @@ void Comp_and_store(float* X, float* Y){
 
   int dist_loc = distence3D(X,Y);
 
-  for(int m = 0; m < 3464; m++) {
-    if(dist[m] == dist_loc) {
-      Frequens[m]++;
-      //printf("Freq++ => %i  dist_loc = %i dist[m] = %i \n",Frequens[m],dist_loc, dist[m]);
-      break;
-    }
-  }
+  //for(int m = 0; m < 3464; m++) {//TODO kolla 3464
+    //if(dist[m] == dist_loc) {
+    //if(m == dist_loc) {
+  Frequens[dist_loc]++;
+
+      //break;
+    //}
+  //}
 };
 
 
@@ -48,23 +51,28 @@ int main(int argc, char *argv[]) {
   FILE * fp1 = fopen("cell_data/cell_web","r");
   FILE * fp2 = fopen("cell_data/cell_web","r");
 
-  // allokerar minne till data
+  fseek(fp1, 0, SEEK_END);
 
-  int max_load = 30/tn/sizeof(float); //max data memory
+  long int end_Of_File = ftell(fp1);
+
+  fseek(fp1, 0, SEEK_SET);
+
+  int tot_row = end_Of_File/23;
+
+  // allokerar minne till data
+  int max_load = 5000000/tn/sizeof(float); //max data memory
 
   int max_row = max_load/3;
 
   float data[max_row][3];
 
-  //resultat
-  int dist_loc;
+  //dist = (int*)malloc((3464)*sizeof(int));
+  //dist =
+  //int Frequens[3465];
 
-  dist = (int*)malloc((3464)*sizeof(int));
-  Frequens = (int*)malloc((3464)*sizeof(int));
-
-  for(int i = 0; i < 3464; i++){
+  for(int i = 0; i < 3465; i++){
     //dist[i] = i*0.01;//Float
-    dist[i] = i;//Int
+    //dist[i] = i;//Int
     Frequens[i] = 0;
   }
 
@@ -72,22 +80,23 @@ int main(int argc, char *argv[]) {
 
   char* etc;
 
+  //Ta reda på hur lång filen är och för for ist.(Snabbare)
+
   //#pragma omp parallel shared(Frequens) {
-  //while(ftell(fp1) != EOF){
-  while (fscanf(fp1, "%c",data) != EOF) {
+  for (int v = 0; v < tot_row; v++){
+  //while (fscanf(fp1, "%c",data) != EOF) {
     fseek(fp1, -1, SEEK_CUR);
-    //fseek(fp1, 0, SEEK_SET);
-    //fseek(fp2, 0, SEEK_SET);
+
     long int poss = ftell(fp1);
 
     printf("Poss = %i\n",poss);
     fseek(fp2, poss, SEEK_SET);
 
     // läser in data 1
-    printf("Data 1:\n");
+    //printf("Data 1:\n");
     for(int i = 0; i<max_row; i++) {
-
       fseek(fp1, +1, SEEK_CUR);
+
       if(fscanf(fp1, "%c",data) != EOF){
         fseek(fp1, -2, SEEK_CUR);
 
@@ -95,7 +104,7 @@ int main(int argc, char *argv[]) {
         fscanf(fp1, "%f" ,&data[i][1]);
         fscanf(fp1, "%f" ,&data[i][2]);
 
-        printf("(%f, %f ,%f)\n",data[i][0],data[i][1],data[i][2]);
+        //printf("(%f, %f ,%f)\n",data[i][0],data[i][1],data[i][2]);
 
         lenData1 = i+1;
       }else{printf("Klar data 1\n");break;}
@@ -109,8 +118,10 @@ int main(int argc, char *argv[]) {
       //fseek(fp2, k*24+24, SEEK_SET);
       fseek(fp2, k*24+24, SEEK_CUR);
 
-      printf("Data 2: (%f %f %f) k = %i lenData1 = %i\n",data[k][0],data[k][1],data[k][2],k,lenData1);
+      //printf("Data 2: (%f %f %f) k = %i lenData1 = %i\n",data[k][0],data[k][1],data[k][2],k,lenData1);
 
+      //Börja vid första ellemntet i data1 och gå till EOF
+      //for(int w = 0; w < tot_row; w++) {
       while (fscanf(fp2, "%c",data) != EOF) {
         fseek(fp2, -2, SEEK_CUR);
 
@@ -118,7 +129,7 @@ int main(int argc, char *argv[]) {
         fscanf(fp2, "%f" ,&y_loc[1]);
         fscanf(fp2, "%f" ,&y_loc[2]);
 
-        printf("(%f, %f ,%f)\n",y_loc[0],y_loc[1],y_loc[2]);
+        //printf("(%f, %f ,%f)\n",y_loc[0],y_loc[1],y_loc[2]);
 
         Comp_and_store(data[k], y_loc);
 
@@ -126,14 +137,14 @@ int main(int argc, char *argv[]) {
       }
     }
   }
-
+  //}
   fclose(fp1);
   fclose(fp2);
 
   //printar resultat
   for(int i = 0; i < 3464; i++){
     if(Frequens[i] != 0){
-      printf("R = %f ",dist[i]*0.01);
+      printf("R = %f ",i*0.01);
       printf("F = %i \n",Frequens[i]);
     }
   }
