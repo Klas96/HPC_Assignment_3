68f31b0d5d98c19b0609f4c43f7ef5b88a1bcd2d
Klas Holmgren <Klas Holmgren>
2018-10-18 14:37:20 +0200

    Parallell but slightly wrong

--- distance_cell.c
+++ distance_cell.c
@@ -6,13 +6,23 @@
 #include<string.h>
 #include<time.h>
 #include<math.h>
-
+/*
+// mäta tid
+//struct timespec start;
+//#define CHECKPOINT(fmt, ...) { \
+struct timespec now; \
+timespec_get(&now, TIME_UTC); \
+float elapsed = (now.tv_sec + 1.0e-9 * now.tv_nsec) - (start.tv_sec + 1.0e-9 * start.tv_nsec); \
+printf("\n%f: ", elapsed); \
+printf(fmt, ##__VA_ARGS__); \
+}
+*/
 int freq[3465];
 
 void Comp_and_store(float* X, float* Y) {
 
   float dist_fun = (X[0]-Y[0])*(X[0]-Y[0]) + (X[1]-Y[1])*(X[1]-Y[1]) + (X[2]-Y[2])*(X[2]-Y[2]);
-  int dist_loc = (int)(((sqrt(dist_fun)+0.005)*100));
+  int dist_loc = (int)(((sqrt(dist_fun))*100)+0.5);
 
   freq[dist_loc]++;
 };
@@ -35,6 +45,10 @@ int main(int argc, char *argv[]) {
   FILE * fp1 = fopen("cell_data/cell_e5","r");
   FILE * fp2 = fopen("cell_data/cell_e5","r");
 
+  //FILE * fp1 = fopen("cell_data/cell_e4","r");
+  //FILE * fp2 = fopen("cell_data/cell_e4","r");
+
+
   //FILE * fp1 = fopen("cell_data/cell_web","r");//Testing
   //FILE * fp2 = fopen("cell_data/cell_web","r");//Testing
 
@@ -44,8 +58,8 @@ int main(int argc, char *argv[]) {
 
   fseek(fp1, 0, SEEK_SET);
 
-  int max_load = 500000/tn/sizeof(float); //max data memory
-  //int max_load = 40/sizeof(float); //TESTING
+  int max_load = 100000000/tn/sizeof(float); //max data memory
+  //int max_load = 80/sizeof(float); //TESTING
   //int lenData1 = 0;
 
   int chunkSize = max_load/6;
@@ -69,14 +83,15 @@ int main(int argc, char *argv[]) {
   int rows1;
   int rows2;
 
-  printf("chunkNr = %i,restRows = %i, tot_row = %i, chunkSize = %i \n",chunkNr,restRows, tot_row, chunkSize );
+  //printf("chunkNr = %i,restRows = %i, tot_row = %i, chunkSize = %i \n",chunkNr,restRows, tot_row, chunkSize );
 
   //Lopar över chunks
-  #pragma omp parallel for reduction(+:freq)
+  //CHECKPOINT("Börjar chunk loop\n");
+  #pragma omp parallel for reduction(+:freq[:3465])
   for(int v = 0; v < chunkNr+1; v++){
 
     int nx = omp_get_thread_num();
-    printf("Thread %i: I take row %i\n",nx);
+    printf("Thread %i: I take chunk %i\n",nx,v);
 
     if(v<chunkNr){
       rows1 = chunkSize;
@@ -84,39 +99,55 @@ int main(int argc, char *argv[]) {
       rows1 = restRows;
     }
 
-
     //Läser Chunk 1
-    //CHECKPOINT("chunk %i\n",v);
+    //printf("Läser in Chunk 1:\n");
     for(int i = 0; i<rows1; i++){
 
-      fscanf(fp1, "%f %f %f" ,&data1[i][0],&data1[i][1],&data1[i][2]);
-      //printf("rad %i (%f, %f ,%f)\n",(v*chunkSize+i),data[i][0],data[i][1],data[i][2]);
+      fscanf(fp1, "%f %f %f\n" ,&data1[i][0],&data1[i][1],&data1[i][2]);
+      //printf("%f %f %f\n", data1[i][0],data1[i][1],data1[i][2]);
+
     }
 
-    fseek(fp2, v*24*chunkSize, SEEK_SET);
+    //Jämför med sig själv
+    for(int j = 0; j < rows1; j++){
+      for(int k = j; k <rows1; k++){
+        Comp_and_store(data1[j],data1[k]);
+      }
+    }
+
+
+    fseek(fp2, (v+1)*24*chunkSize, SEEK_SET);
 
     //Jmf Chunk 1 med alla chunks
-    for(int j = v; j <  chunkNr+1; j++){
+    for(int j = v+1; j <  chunkNr+1; j++){
       if(j<chunkNr){
         rows2 = chunkSize;
       }else{
         rows2 = restRows;
+
       }
 
       //Läser Chunk 2
+      //printf("Läser in Cunk 2: \n ");
       for(int i = 0; i<rows2; i++){
         fscanf(fp2, "%f %f %f" ,&data2[i][0],&data2[i][1],&data2[i][2]);
+        //printf("%f %f %f\n", data2[i][0],data2[i][1],data2[i][2]);
       }
 
       //compare Chunk1 och Chunk2
+      //printf("rows1 = %i, rows2 = %i\n", rows1, rows2);
       for(int k = 0; k<rows1; k++){
-        for(int l = k; l < rows2; l++){
-          //Comp_and_store(data1[k],data2[l]);
+        for(int l = 0; l < rows2; l++){
+          //printf("Jämför: ");
+          //printf("D1: %f %f %f och ", data1[k][0],data1[k][1],data1[k][2]);
+          //printf("D2: %f %f %f\n", data2[l][0],data2[l][1],data2[l][2]);
+          Comp_and_store(data1[k],data2[l]);
 
-          float dist_fun = (data1[0]-data2[0])*(data1[0]-data2[0]) + (data1[1]-data2[1])*(data1[1]-data2[1]) + (data1[2]-data2[2])*(data1[2]-data2[2]);
-          int dist_loc = (int)(((sqrt(dist_fun)+0.005)*100));
+          //float dist_fun = (data1[0]-data2[0])*(data1[0]-Y[0]) + (data1[1]-Y[1])*(data1[1]-Y[1]) + (data1[2]-Y[2])*(data1[2]-Y[2]);
+          //int dist_loc = (int)(((sqrt(dist_fun)+0.005)*100));
 
-          freq[dist_loc]++;
+          //freq[dist_loc]++;
+
         }
       }
 
